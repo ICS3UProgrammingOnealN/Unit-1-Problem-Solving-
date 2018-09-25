527a991919538356fbbf4a504bf817ea48c235c3
OnealNnah <s450723@ocsb.local>
2018-09-25 11:13:30 -0400

    Setup form

--- CircumfOneal/CircumfOneal/CircumForm.Designer.cs
+++ CircumfOneal/CircumfOneal/CircumForm.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblCircumference = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.lblCircumferenceAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(62, 70);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(197, 25);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "What is the radius?";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(206, 142);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(115, 45);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblCircumference
+            // 
+            this.lblCircumference.AutoSize = true;
+            this.lblCircumference.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCircumference.Location = new System.Drawing.Point(95, 238);
+            this.lblCircumference.Name = "lblCircumference";
+            this.lblCircumference.Size = new System.Drawing.Size(227, 25);
+            this.lblCircumference.TabIndex = 2;
+            this.lblCircumference.Text = "The Ciircumference is:";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRadius.Location = new System.Drawing.Point(265, 67);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 31);
+            this.txtRadius.TabIndex = 3;
+            // 
+            // lblCircumferenceAnswer
+            // 
+            this.lblCircumferenceAnswer.AutoSize = true;
+            this.lblCircumferenceAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCircumferenceAnswer.Location = new System.Drawing.Point(344, 238);
+            this.lblCircumferenceAnswer.Name = "lblCircumferenceAnswer";
+            this.lblCircumferenceAnswer.Size = new System.Drawing.Size(24, 25);
+            this.lblCircumferenceAnswer.TabIndex = 4;
+            this.lblCircumferenceAnswer.Text = "?";
+            // 
+            // CircumForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 298);
+            this.Controls.Add(this.lblCircumferenceAnswer);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblCircumference);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "CircumForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblCircumference;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label lblCircumferenceAnswer;
     }
 }
 
--- CircumfOneal/CircumfOneal/CircumForm.cs
+++ CircumfOneal/CircumfOneal/CircumForm.cs
@@ -16,5 +16,10 @@ namespace CircumfOneal
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
